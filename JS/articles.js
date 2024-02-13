a3ed23e905e08a6c5c8000af3ef808aad7d0cb5b
skuullking <146060891+skuullking@users.noreply.github.com>
2024-02-13 09:04:08 +0100

    update articles js

--- JS/articles.js
+++ JS/articles.js
@@ -1,34 +1,158 @@
 let reponse = [
     {
       _id: "a001",
-      nomProduit: "PE",
+      nomProduit: "Super Nintendo Classic",
       prix: 80,
     },
     {
       _id: "a002",
-      nomProduit: "SA",
+      nomProduit: "Nintendo 64",
+      prix: 500,
+    },
+    {
+      _id: "a003",
+      nomProduit: "Nintendo GameCube",
+      prix: 500,
+    },
+    {
+      _id: "a004",
+      nomProduit: "Nintendo DS",
+      prix: 500,
+    },
+    {
+      _id: "a005",
+      nomProduit: "Nintendo Wii",
+      prix: 500,
+    },
+    {
+      _id: "a006",
+      nomProduit: "Nintendo 3DS",
+      prix: 500,
+    },
+    {
+      _id: "a007",
+      nomProduit: "Nintendo Wii U",
+      prix: 500,
+    },
+    {
+      _id: "a008",
+      nomProduit: "Nintendo Switch",
       prix: 500,
     },
     {
       _id: "b001",
-      nomProduit: "SRA",
+      nomProduit: "PlayStation 1",
+      prix: 50,
+    },
+    
+    {
+      _id: "b002",
+      nomProduit: "PlayStation 2",
       prix: 50,
     },
     {
-      _id: "d001",
-      nomProduit: "MAV",
-      prix: 90,
+      _id: "b003",
+      nomProduit: "PlayStation 3",
+      prix: 50,
     },
     {
-      _id: "c002",
-      nomProduit: "GEA",
-      prix: 25,
+      _id: "b004",
+      nomProduit: "PlayStation 4",
+      prix: 50,
+    },
+    {
+      _id: "b005",
+      nomProduit: "PlayStation 5",
+      prix: 50,
+    },
+    {
+      _id: "b006",
+      nomProduit: "PS Vita",
+      prix: 50,
+    },
+    {
+      _id: "b007",
+      nomProduit: "PSP",
+      prix: 50,
+    },
+    {
+      _id: "b008",
+      nomProduit: "PlayStation VR",
+      prix: 50,
     },
     {
       _id: "c001",
-      nomProduit: "MAX",
-      prix: 10,
+      nomProduit: "Xbox 360",
+      prix: 25,
     },
-  ];
-
-//les article du qui peuvent être mis dans le panier
+    {
+      _id: "c002",
+      nomProduit: "Xbox one",
+      prix: 25,
+    },
+    {
+      _id: "c003",
+      nomProduit: "Xbox one x",
+      prix: 25,
+    },
+    {
+      _id: "c004",
+      nomProduit: "Xbox series s",
+      prix: 25,
+    },
+    {
+      _id: "c005",
+      nomProduit: "xbox series x",
+      prix: 25,
+    },
+    {
+      _id: "c006",
+      nomProduit: "manette xbox series x et s",
+      prix: 25,
+    },
+    {
+      _id: "c007",
+      nomProduit: "manette xbox elite",
+      prix: 25,
+    },
+    {
+      _id: "c008",
+      nomProduit: "casque xbox",
+      prix: 25,
+    },
+    {
+      _id: "d001",
+      nomProduit: "Clavier",
+      prix: 90,
+    },
+    {
+      _id: "d002",
+      nomProduit: "Enceinte Gaming",
+      prix: 90,
+    },
+    {
+      _id: "d003",
+      nomProduit: "Fauteuil Gaming",
+      prix: 90,
+    },
+    {
+      _id: "d004",
+      nomProduit: "Casque Gaming",
+      prix: 90,
+    },
+    {
+      _id: "d005",
+      nomProduit: "Souris Gaming",
+      prix: 90,
+    },
+    {
+      _id: "d006",
+      nomProduit: "Tapis de souris Gaming",
+      prix: 90,
+    },
+    {
+      _id: "d007",
+      nomProduit: "Bureau Gaming",
+      prix: 90,
+    },
+  ]
